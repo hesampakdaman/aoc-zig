865eb97cac9c2192bf9c808f649b22d6dafe0b72
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2025-07-17 07:45:20 +0200

    fix buffer

--- src/year2015/day02.zig
+++ src/year2015/day02.zig
@@ -5,10 +5,11 @@ const Solution = @import("../solution.zig").Solution;
 pub fn solve(allocator: std.mem.Allocator, input: []const u8) !Solution {
     const tick = try std.time.Instant.now();
 
-    var boxes: [1000]Box = undefined;
-    try Box.parse(&boxes, input);
-    const p1 = part1(&boxes);
-    const p2 = part2(&boxes);
+    var buf: [3000]u64 = undefined;
+    const boxes = try parse(&buf, input);
+
+    const p1 = part1(boxes);
+    const p2 = part2(boxes);
 
     const tock = try std.time.Instant.now();
 
@@ -21,54 +22,42 @@ pub fn solve(allocator: std.mem.Allocator, input: []const u8) !Solution {
     );
 }
 
-fn part1(dims: []const Box) u64 {
+fn part1(input: []const u64) u64 {
     var paper: u64 = 0;
-    for (dims) |dim| {
-        const area = 2 * (dim.min * dim.mid + dim.mid * dim.max + dim.max * dim.min);
-        const slack = dim.min * dim.mid;
+    var it = std.mem.window(u64, input, 3, 3);
+    while (it.next()) |box| {
+        const area = 2 * (box[0] * box[1] + box[0] * box[2] + box[1] * box[2]);
+        const slack = box[0] * box[1];
         paper += area + slack;
     }
     return paper;
 }
 
-fn part2(input: []const Box) u64 {
+fn part2(input: []const u64) u64 {
     var ribbon: u64 = 0;
-    for (input) |box| {
-        const cubic = box.mid * box.min * box.max;
-        const perimiter = 2 * (box.mid + box.min);
+    var it = std.mem.window(u64, input, 3, 3);
+    while (it.next()) |box| {
+        const cubic = box[0] * box[1] * box[2];
+        const perimiter = 2 * (box[1] + box[0]);
         ribbon += cubic + perimiter;
     }
     return ribbon;
 }
 
-const Box = struct {
-    min: u64,
-    mid: u64,
-    max: u64,
+fn parse(buf: []u64, input: []const u8) ![]u64 {
+    const read = try utils.parseU64s(buf, input);
+    const numbers = buf[0..read];
 
-    fn parse(boxes: []Box, input: []const u8) !void {
-        var buf: [3000]u64 = undefined;
-        const read = try utils.parseU64s(&buf, input);
-        const numbers = buf[0..read];
+    var i: usize = 0;
+    while (i + 2 < numbers.len) : (i += 3) {
+        var chunk = numbers[i .. i + 3];
+        const min = @min(chunk[0], chunk[1], chunk[2]);
+        const max = @max(chunk[0], chunk[1], chunk[2]);
+        const mid = chunk[0] + chunk[1] + chunk[2] - min - max;
 
-        var it = std.mem.window(u64, numbers, 3, 3);
-        var i: usize = 0;
-        while (it.next()) |chunk| {
-            const min = @min(chunk[0], chunk[1], chunk[2]);
-            const max = @max(chunk[0], chunk[1], chunk[2]);
-            const mid = chunk[0] + chunk[1] + chunk[2] - min - max;
-            boxes[i] = .{ .min = min, .mid = mid, .max = max };
-            i += 1;
-        }
+        chunk[0] = min;
+        chunk[1] = mid;
+        chunk[2] = max;
     }
-};
-
-test "part1" {
-    const dims = [_]Box{Box{ .min = 2, .mid = 3, .max = 4 }};
-    try std.testing.expectEqual(58, part1(&dims));
-}
-
-test "part2" {
-    const dims = [_]Box{Box{ .min = 2, .mid = 3, .max = 4 }};
-    try std.testing.expectEqual(34, part2(&dims));
+    return numbers;
 }
