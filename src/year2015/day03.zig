0de83c48cab69d86051e4f36dd4edaf30767398b
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2025-07-26 15:33:11 +0200

    simplify

--- src/year2015/day03.zig
+++ src/year2015/day03.zig
@@ -4,27 +4,8 @@ const Solution = @import("../solution.zig").Solution;
 pub fn solve(allocator: std.mem.Allocator, input: []const u8) !Solution {
     const tick = try std.time.Instant.now();
 
-    var x_min: i32 = 1;
-    var x_max: i32 = -1;
-    var y_min: i32 = 1;
-    var y_max: i32 = -1;
-
-    var pos: [2]i32 = .{ 0, 0 };
-    for (input) |char| {
-        x_min = @min(x_min, pos[0]);
-        x_max = @max(x_max, pos[0]);
-        y_min = @min(y_min, pos[1]);
-        y_max = @max(y_max, pos[1]);
-        switch (char) {
-            '>' => pos = .{ pos[0] + 1, pos[1] },
-            '<' => pos = .{ pos[0] - 1, pos[1] },
-            '^' => pos = .{ pos[0], pos[1] + 1 },
-            else => pos = .{ pos[0], pos[1] - 1 },
-        }
-    }
     var set = Set.init(allocator);
-    try set.ensureTotalCapacity(10000);
-    defer set.deinit();
+    try set.ensureTotalCapacity(@intCast(input.len));
 
     const p1 = try part1(&set, input);
     const p2 = 3;
@@ -56,8 +37,7 @@ fn part1(set: *Set, input: []const u8) !usize {
             '>' => pos = .{ pos[0] + 1, pos[1] },
             '<' => pos = .{ pos[0] - 1, pos[1] },
             '^' => pos = .{ pos[0], pos[1] + 1 },
-            'v' => pos = .{ pos[0], pos[1] - 1 },
-            else => std.debug.print("Unknown direction: {c}\n", .{dir}),
+            else => pos = .{ pos[0], pos[1] - 1 },
         }
     }
     return houses;
